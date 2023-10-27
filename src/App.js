c14dcae3d9e3a98bb867995a376068d831061264
Thirupathi <thirupathipathuri234@gmail.com>
2023-10-27 11:51:53 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+    <h1 className="title">Super Over League</h1>
+    <div className="image-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image" alt="rcb"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image" alt="csk"
+      />
     </div>
-  );
+  </div>
+);
+  
 }
 
 export default App;
